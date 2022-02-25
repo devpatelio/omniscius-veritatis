ac61e2da2ed1d2dd03725aea46f5ac06254399f4
Dev Patel <devpatelio@Develios-MacBook-Pro.local>
2022-02-24 23:49:01 -0500

    remove src extra files

--- src/sentiment.py
+++
@@ -1,9 +0,0 @@
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
-from nltk.sentiment import SentimentAnalyzer, SentimentIntensityAnalyzer
-from nltk.sentiment.util import *
-import nltk
-
-def sentiment(inp_text):
-    sia = SentimentIntensityAnalyzer()
-    return sia.polarity_scores(inp_text)
